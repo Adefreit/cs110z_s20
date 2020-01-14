c9c5c07a9dee55fd56068d61443feb3f09f70110
Adefreit <Adefreit@users.noreply.github.com>
2020-01-14 09:39:45 -0700

    Adding lesson 3's examples

---
+++ l3_print.py
@@ -0,0 +1,31 @@
+# ---------------------------------------------------------------
+# Printing Examples
+# CS110Z - Lesson 3
+# ---------------------------------------------------------------
+
+# Printing a Simple Message
+print("Hello World")
+
+# Printing Multiple, Independent Strings (each one separated by a space)
+print("Hello", "How", "Are", "You?")
+
+# Spaces not in quotation marks are ignored by python
+print("Hello",               "World")
+
+# Everything in the quotation mark prints out EXACTLY as written
+print("1 + 2")
+
+# If you want to print a mathematical expression, don't put in quotes
+print(1 + 2)
+
+# The Newline Character \n is used to add new lines.
+print("Hello\nHow\nAre\nYou")
+
+# The following is a variable
+name = "Bob"
+
+# You can print variables (just don't put the variable in quotes!)
+print(name)
+
+# Finally, you can print any combination of strings, variables, and math expressions
+print("Hello", name, "how are you? Did you know that 1+1 =", 1+1)
